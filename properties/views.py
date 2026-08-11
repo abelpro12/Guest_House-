b9b8e5161e5a199ce4498abad2710094b84e381c
GuestHouse Developer <developer@guesthouse.local>
2026-08-11 15:07:09 +0300

    Fix NameError in properties/views.py by importing Q from django.db.models

--- properties/views.py
+++ properties/views.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 from .models import Property, PropertyStaff
 from accounts.models import CustomUser
 
